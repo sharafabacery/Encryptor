43bcc771546a0d1c5a27a3e73df2ae05712a8c11
sharaf eldien ehab <52586357+sharafabacery@users.noreply.github.com>
2023-08-28 16:58:23 +0300

    update

--- Encryptor/Decryption.cs
+++ Encryptor/Decryption.cs
@@ -6,8 +6,7 @@ namespace Encryptor
     class Decryption
     {
         private byte[] IV = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
-        private int BlockSize = 128;
-        public  void DecryptFile(string filePath,byte[]key)
+         public  void DecryptFile(string filePath,byte[]key)
     {
         string tempFile = filePath + ".temp";
 
--- Encryptor/Encryption.cs
+++ Encryptor/Encryption.cs
@@ -6,8 +6,7 @@ namespace Encryptor
     class Encryption
     {
         private byte[] IV = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
-        private int BlockSize = 128;
-        
+       
         
         public void EncryptFile(string filePath,byte[]key)
     {
