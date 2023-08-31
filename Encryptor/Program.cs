77a33bb46616f2166d0012d5a4cdf77d55fd8d07
sharaf eldien ehab <52586357+sharafabacery@users.noreply.github.com>
2023-08-31 14:24:20 +0300

    Update Program.cs

--- Encryptor/Program.cs
+++ Encryptor/Program.cs
@@ -70,39 +70,6 @@ namespace Encryptor
 
             }
 
-
-            //EncryptorController encryptorController =new EncryptorController("a7a7a7a7a7a7a7a7a7");
-            // encryptorController.ApplyEncryptionDirectories("C:\\Users\\Sharaf Elden\\Desktop\\New folder (2)");
-            //encryptorController.ApplyDecryptionDirectories("C:\\Users\\Sharaf Elden\\Desktop\\New folder (2)");
-
-            /*Keys keys =new Keys();
-            keys.Key="SGVsbG8gdGhlcmUh";
-            DirectoryBuilder directoryBuilder =new DirectoryBuilder();
-            directoryBuilder
-            .DirectoryPath="C:\\Users\\Sharaf Elden\\Downloads\\New folder";
-            directoryBuilder.DirectoryFiles(directoryBuilder.DirectoryPath);
-
-            Encryption encryption =new Encryption();
-            foreach (var item in directoryBuilder.files)
-            {
-            encryption.EncryptFile(item,keys.KeyByte());
-                
-            }
-            ///encryption.EncryptFile("F:\\.Net Stack ITI\\projects\\Encryptor\\Encryptor\\38.mp4",keys.KeyByte());
-            //Decryption decryption=new Decryption();
-            //decryption.DecryptFile("F:\\.Net Stack ITI\\projects\\Encryptor\\Encryptor\\38.mp4",keys.KeyByte());
-           
-            DirectoryNavigator directoryNavigator=new DirectoryNavigator();
-            directoryNavigator.GetSubDirectoriesofDirectory("F:\\sql server");
-            foreach (var item in directoryNavigator.Directories)
-            {
-                System.Console.WriteLine("Directory name: "+item.DirectoryPath);
-                foreach(var item2 in item.files){
-                    System.Console.WriteLine("File name: "+item2);
-                }
-            } */
-            //File.Delete("F:\\New folder (3)\\New Bitmap Image.bmp");
-
         }
     }
 }
