e0b7db465bc509c5a17a7193aed3019bc6983280
sharaf eldien ehab <52586357+sharafabacery@users.noreply.github.com>
2023-08-31 11:06:13 +0300

    Create ConsoleDisplay.cs

---
+++ Encryptor/ConsoleDisplay.cs
@@ -0,0 +1,10 @@
+using System;
+namespace Encryptor
+{
+    public static class ConsoleDispaly{
+        public static void WelcomeDispaly(){
+            System.Console.WriteLine("Welcome to Encryptor");
+
+        }
+    }
+}
